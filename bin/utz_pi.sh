a2134e04872c90b2488c0ff9b60c443a7d4989fb
cornicis <cornicis@gmail.com>
2014-10-18 18:07:31 +0000

    update starters for new utz name

--- bin/utz_pi.sh
+++ bin/utz_pi.sh
@@ -1,1 +1,1 @@
-pd-extended -noadc -audiooutdev 1,2 -alsa -midiindev 1 -nomidiout -alsamidi /home/pi/puredata/patches/gui/workspaceMPK.pd
+pd-extended -noadc -audiooutdev 1,2 -alsa -midiindev 1 -nomidiout -alsamidi /home/pi/puredata/patches/utz.pd
