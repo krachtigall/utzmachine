f342d0bf2252b74f105834aae7f63f820b8969b0
krachtigall <cornicis@googlemail>
2015-04-09 20:29:34 +0200

    make it work for rpi2

--- bin/utz_ssh.sh
+++ bin/utz_ssh.sh
@@ -1,1 +1,1 @@
-pd-extended -nogui -noadc -audiooutdev 1,2 -alsa -midiindev 1 -nomidiout -alsamidi /home/pi/puredata/patches/utz.pd
+puredata -nogui -noadc -audiooutdev 1,2 -alsa -midiindev 1 -nomidiout -alsamidi /home/pi/utzmachine/src/utz.pd
