46f88a4c99a37cb6d28551ade2b7f6bfba28fc08
cornicis <cornicis@gmail.com>
2015-05-09 20:31:38 +0200

    change install scripts because ip/hostname is different for direct connection

--- bin/install.sh
+++
@@ -1,1 +0,0 @@
-rsync -r -z --progress ../../utzmachine pi@192.168.1.155:/home/pi/ || (echo; echo 'Error on install'; read enter); exit
---
+++ bin/install_router.sh
@@ -0,0 +1,1 @@
+rsync -r -z --progress ../utzmachine pi@utzmachine:/home/pi/ || (echo; echo 'Error on install'; read enter); exit
