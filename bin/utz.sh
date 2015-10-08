074b25a69f7eb16b1b42e43a995c570b11bf205d
cornicis <cornicis@gmail.com>
2015-10-09 00:51:43 +0200

    wrong filename for patchbay. MIDI connection works on startup again!

--- bin/utz.sh
+++ bin/utz.sh
@@ -1,2 +1,2 @@
-qjackctl --active-patchbay=/home/pi/utzmachine/puredata/bin/patchbay.xml &
+qjackctl --active-patchbay=/home/pi/utzmachine/bin/patchbay.xml &
 puredata -nogui -noadc -audiooutdev 1,2 -alsa -midiindev 1 -nomidiout -alsamidi /home/pi/utzmachine/puredata/utz.pd
