0faf0d610857d8e9c3a61df0ec8bb02a6f24331d
cornicis <cornicis@googlemail.com>
2016-05-16 17:06:21 +0200

    change binary name to pd and add a gui start script.

--- bin/utz.sh
+++ bin/utz.sh
@@ -1,2 +1,2 @@
 qjackctl --active-patchbay=/home/pi/utzmachine/bin/patchbay.xml &
-puredata -nogui -noadc -audiooutdev 1,2 -alsa -midiindev 2 -nomidiout -alsamidi /home/pi/utzmachine/puredata/utz.pd 2> /home/pi/utzmachine/log/pd.log &
+pd -nogui -noadc -audiooutdev 1,2 -alsa -midiindev 2 -nomidiout -alsamidi /home/pi/utzmachine/puredata/utz.pd 2> /home/pi/utzmachine/log/pd.log &
---
+++ bin/utz_gui.sh
@@ -0,0 +1,2 @@
+qjackctl --active-patchbay=/home/pi/utzmachine/bin/patchbay.xml &
+pd -noadc -audiooutdev 1,2 -alsa -midiindev 2 -nomidiout -alsamidi /home/pi/utzmachine/puredata/utz.pd 2> /home/pi/utzmachine/log/pd.log &
